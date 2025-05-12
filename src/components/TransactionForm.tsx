d4f76689d53ad7375f4b8580ee14453bfb5d1cbe
Yaniv Daye <yaniv120892@gmail.com>
2025-05-12 16:46:29 +0300

    fix eslint

--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -14,7 +14,6 @@ import {
 import { Category, CreateTransactionInput, Transaction } from "../types";
 import { getCategories } from "../services/transactions";
 import dayjs from "dayjs";
-import { on } from "events";
 
 type Props = {
   open: boolean;

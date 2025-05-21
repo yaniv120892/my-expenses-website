4ec535d62438d556901cbc478a0bcd2ce2a98324
Yaniv Daye <yaniv120892@gmail.com>
2025-05-21 16:07:28 +0300

    fix skeletons

--- src/components/SummaryChart.tsx
+++ src/components/SummaryChart.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography, CircularProgress, Paper } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 import {
   BarChart,
   Bar,

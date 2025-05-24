5e5bc70a551a0b61939ed3bf28aa6834e44dd4ff
Yaniv Daye <yanivdaye@artlist.io>
2025-05-24 15:16:16 +0300

    fix light theme colors

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -15,12 +15,14 @@ export default function EmptyState({ message }: Props) {
       justifyContent="center"
       minHeight={180}
       gap={1.5}
-      sx={{ color: "white", opacity: 0.8, textAlign: "center" }}
+      sx={{
+        opacity: 0.8,
+        textAlign: "center",
+      }}
+      color={"var(--text-color)"}
     >
-      <InfoOutlinedIcon sx={{ fontSize: 48, opacity: 0.6, color: "inherit" }} />
-      <Typography variant="subtitle1" sx={{ opacity: 0.8, fontWeight: 500 }}>
-        {message}
-      </Typography>
+      <InfoOutlinedIcon sx={{ fontSize: 48, opacity: 0.6 }} />
+      <Typography sx={{ opacity: 0.8, fontWeight: 500 }}>{message}</Typography>
     </Box>
   );
 }

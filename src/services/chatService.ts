a32acdcd325edc7aa4ef7f4210968e986691261b
Yaniv Daye <yaniv.daye@artlist.io>
2026-07-11 23:08:32 +0300

    feat(agent): add agent chat confirmation UI

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -1,10 +1,4 @@
-import api from './api';
-
-import { Message } from '../hooks/useChat';
-
-export const sendMessage = async (messages: Message[]): Promise<string> => {
-  const response = await api.post('/api/chat', { messages });
-  console.log(response);
-  return response.data;
-};
-
+export {
+  confirmAgentPendingAction,
+  sendAgentMessage,
+} from "@/services/agentService";
